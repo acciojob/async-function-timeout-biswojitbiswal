1ba8855988c07950ac51524aaa1f29fc91c7bdf2
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-11-05 12:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let inputText = document.getElementById("text");
+let inputNum = document.getElementById("delay");
+let btn = document.getElementById("btn");
+let box = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+    setTimeout(() => {
+        box.textContent = inputText.value;
+    }, inputNum.value);
+})
